005fe36e734ee9c698df68965bbe7ec281c10e8b
Kononovkirill <43089483+Kononovkirill@users.noreply.github.com>
2018-09-21 14:00:57 +0300

    add 4laba

---
+++ 4laba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xn = 0.1,xk = 0.5, h = 0.4,x,Yx;
+            x = xn;
+            do
+            {
+                Yx = Math.Atan(x);
+                x += h;
+                Console.Write(Yx+"\n");
+
+            } while (x<=xk+h/2);
+
+            Console.ReadKey();
+        }
+    }
+}
+
