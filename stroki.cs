542b7abe857e2e99f47f385f6ad999465488ce6a
Kononovkirill <43089483+Kononovkirill@users.noreply.github.com>
2018-09-21 13:57:58 +0300

    add stkoki

---
+++ stroki.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_xx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a character: ");
+            char c = (char)Console.Read();
+            if (Char.IsLetter(c))
+            {
+                if (Char.IsLower(c))
+                {
+                    Console.WriteLine("The character is lowercase.");
+                }
+                else
+                {
+                    Console.WriteLine("The character is uppercase.");
+                }
+            }
+
+            if (Char.IsNumber(c))
+                Console.WriteLine("Its a number. ");
+
+            if (Char.IsPunctuation(c))
+                Console.WriteLine("Its a punctuation. ");
+
+            if (Char.IsSeparator(c))
+                Console.WriteLine("Its a separator. ");
+
+            if (Char.IsWhiteSpace(c))
+                Console.WriteLine("Its a separator. ");
+
+            Console.ReadKey();
+     
+        }
+    }
+}
