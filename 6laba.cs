7c77e714d0743be51c4b082d5b4d4ad0c580ba78
Kirill7Kononov <43089483+Kononovkirill@users.noreply.github.com>
2018-09-24 14:13:34 +0300

    Add files via upload

---
+++ 6laba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку S1");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Введите строку S2");
+            string s2 = Console.ReadLine();
+            Console.WriteLine("Введите строку s3");
+            string s3 = Console.ReadLine();
+
+            int startIndex = s1.IndexOf(s2);
+            if (startIndex != -1)
+            {
+                Console.WriteLine("Измененная строка: ");
+                Console.WriteLine(s1.Remove(startIndex, s2.Length).Insert(startIndex, s3));
+            }
+            else
+                Console.WriteLine("Строка не изменена");
+          
+            Console.ReadKey();
+        }
+    }
+}
