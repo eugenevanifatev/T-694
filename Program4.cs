f682c36a2e15727322716dd44a3261aa26e16d2a
RiabikovR <43089170+RiabikovR@users.noreply.github.com>
2018-10-03 19:07:45 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KPIAP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //В 16
+            //Подсчитать к - количество цифр в десятичной записи целого неотрицательного числа n
+            int digitCount (int number)
+            {
+                int k = 0;
+                if (number == 0) return 1;
+                while (number > 0)
+                {
+                    k++;
+                    number /= 10;
+                }
+                return k;
+            }
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n" + digitCount(n));
+            Console.ReadKey();
+        }
+    }
+}
